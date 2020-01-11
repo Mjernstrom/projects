4ad08ca1a719b2371cfc136363fd9261a897b88e
Matthew Jernstrom <Mattjernstrom@gmail.com>
2020-01-11 13:25:55 -0600

    Update battleship.cpp

--- battleship.cpp
+++ battleship.cpp
@@ -8,17 +8,31 @@
 //  So the bot will choose to hit another adjacent spot on it's next turn, and it will not choosing the previously missed spot
 //  Once the bot sinks your ship, "hit mode" is turned off. 
 #include <iostream>
+#include <string>
+#include <sstream>
 using namespace std;
 string boardOpp[12][12];
 string boardPlayer[12][12];
 
-void shipPlacement(){
-    string placeInput;
-    if(placeInput[0] == 'A' || placeInput[0] == 'a'){
-        
+void shipPlacement(string placeInput){
+    char letterInputCap;
+    int inputInt;
+    istringstream(placeInput[1])>>inputInt;
+    for(letterInputCap = 65; letterInputCap <= 107; letterInputCap++){
+        if (placeInput[0] == letterInputCap){
+            for(int j = 1; j < 12; j++){
+                if (letterInputCap <= 74){
+                    if (inputInt == j)
+                        boardPlayer[letterInputCap - 64][inputInt] = '+';
+                }
+                else if (letterInputCap >= 97){
+                    if (inputInt == j)
+                        boardPlayer[letterInputCap  - 96][inputInt] = '+';
+                }
+            }
+        }
     }
 }
-
 void displayCpuBoard(){
     cout<<"\n"<<"CPU's board:"<<endl;
     for(int i = 0; i < 12; i++)
@@ -68,7 +82,13 @@ void createBoard(){
             }
         }
 }
-
+bool playGame(){
+    
+    displayPlBoard();
+    displayCpuBoard();
+    
+    return true;
+}
 int main() {
     // Display game instructions
     cout<<"\n"<<"    Welcome to the Battleship game!"<<endl
@@ -78,9 +98,29 @@ int main() {
     <<"\n"<<"    You have four game pieces (2 frigates and two attack boats): "<<endl<<"\n\n";
     cout<<"    1    2    3    4"<<"\n\n"<<"    +    +    +    +"<<endl<<"    +    +    +    +"<<endl<<"    +    +"<<"\n\n";
     cout<<"\n"<<"Place your pieces by entering the Letter followed by the number of the spot such as 'A5'. No diagnal placement! :"<<endl;
+    // Start Game
     createBoard();
-    displayPlBoard();
-
-    cout<<"\n\n";
+    string placeInput;
+    // Handle position input
+    for(int j = 1; j < 5; j++){
+        if(j < 3){
+            for(int i = 1; i < 4; i++){
+                int max = 4 - i;
+                displayPlBoard();
+                cout<<"\n"<<"Enter spot "<<i<<" for frigate #"<<j<<" ("<<max<<" spots left): "<<endl;
+                cin>>placeInput;
+                shipPlacement(placeInput);
+            }
+    }
+        else{
+            for(int i = 1; i < 3; i++){
+                int max = 3 - i;
+                displayPlBoard();
+                cout<<"\n"<<"Enter spot "<<i<<" for attack boat #"<<i<<" ("<<max<<" spots left): "<<endl;
+                cin>>placeInput;
+                shipPlacement(placeInput);
+            }
+        }
+    }
     return 0;
 }
