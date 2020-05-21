2e0bddd2691ea3d548b644c9204c7fbdec923e54
Matthew Jernstrom <Mjerns1@lsu.edu>
2020-05-21 12:08:59 -0500

    Update and rename web.py to webScraper.py

--- webScraper.py
+++ webScraper.py
@@ -9,6 +9,9 @@
 # investing.com
 # nasdaq.com
 
+# A simple scraper that scrapes article links with a desired keyword
+# Only scrapes Investopedia for now
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
 
@@ -26,8 +29,7 @@ def get_Investopedia(keyword):
 
     page_html = company_page.read() + market_page.read() + trading_page.read()
     page_soup = soup(page_html, 'html.parser')
-    page_articles = page_soup.findAll(
-        'a', {'class': 'comp card mntl-card card'}, )
+    page_articles = page_soup.findAll('a', {'class': 'comp card mntl-card card'}, )
     company_page.close()
     market_page.close()
     company_page.close()
@@ -42,9 +44,19 @@ def get_Investopedia(keyword):
     
 
 def main():
-    result = get_Investopedia('earnings')
-    for item in result:
-        print(item)
+    search_loop = True
+    while search_loop:
+        keyword_input = input('Enter string keyword to search for on the website:')
+        result = get_Investopedia(keyword_input)
+        for item in result:
+            print(item)
+        search_input = input('Enter y/n to continue searching. . .')
+        search_input.lower()
+        if search_input == 'n':
+            search_loop = False
+            break
+        elif search_input != 'y':
+            raise Exception('Input must be y/n')
 
 
 main()
